0128447e15913c40be0ce348812d7d88412074ee
Thomas Janko <thomas78125@gmail.com>
2025-10-17 18:00:22 +0200

    nav

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import NavItems from './NavItems'
+
+const Header = () => {
+  return (
+    <header className='sticky top-0 header'>
+        <div className='container header-wrapper'>
+            <Link href='/'>
+                <Image src='/assets/icons/logo.svg' alt='logo' width={140} height={32} className='h-auto w-auto cursor-pointer'/>
+            </Link>
+            <nav className='hidden sm:block'>
+                <NavItems />
+            </nav>
+            {/* UserDropdown  */}
+        </div>
+    </header>
+  )
+}
+
+export default Header
