4979a6e4a6229649470187a2f13cf79b14865da7
Thomas Janko <thomas78125@gmail.com>
2025-10-22 23:28:52 +0200

    Sign in

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="auth-layout">
+      <section className="auth-left-section scrollbar-hide-default">
+        <Link href="/" className="auth-logo">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="logo"
+            width={140}
+            height={32}
+            className="h-8 w-auto cursor-pointer"
+          />
+        </Link>
+        <div className="flex-1 pb-6 lg:pb-8">{children}</div>
+      </section>
+      <section className="auth-right-section">
+        <div className="relative z-10 lg:mt-4 lg:mb-16">
+          <blockquote className="auth-blockquote">
+            Signalist turned my watchlist inta a winning list. The alerts are spot-on, and I feed more confident making
+            trades in the market.
+          </blockquote>
+          <div className="flex items-center justify-between">
+            <div>
+              <cite className="auth-testimonial-author">- Ethan R</cite>
+              <p className="max-md:test-xs text-gray-500">Trader since 2019</p>
+            </div>
+            <div className="flex items-center gap-0.5">
+              {[1, 2, 3, 4, 5].map((star) => (
+                <Image key={star} src="/assets/icons/star.svg" alt="Star" width={20} height={20} className="h-5 w-5" />
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="relative flex-1">
+          <Image
+            src="/assets/images/dashboard.png"
+            alt="Dashboard Preview"
+            width={1440}
+            height={1150}
+            className="auth-dashboard-preview absolute top-0"
+          />
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Layout;
