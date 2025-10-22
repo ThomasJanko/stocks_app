06c2754f5d3d2a1dfe17078929570a4069b86709
Thomas Janko <thomas78125@gmail.com>
2025-10-22 23:59:33 +0200

    InputField + Forms

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,51 @@
+'use client';
+import InputField from '@/components/forms/InputField';
+import { useForm } from 'react-hook-form';
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      fullName: '',
+      email: '',
+      password: '',
+      country: 'US',
+      investmentGoals: 'Growth',
+      riskTolerance: 'Medium',
+      preferredIndustry: 'Technology',
+    },
+    mode: 'onBlur',
+  });
+
+  const onSubmit = async (data: SignUpFormData) => {
+    try {
+      console.log('Signing up with data:', data);
+    } catch (error) {
+      console.error('Error signing up:', error);
+    }
+  };
+  return (
+    <>
+      <h1 className="form-title">Sign Up & Personnalize</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <InputField
+          name="fullName"
+          label="Full Name"
+          placeholder="John Doe"
+          register={register}
+          error={errors.fullName}
+          validation={{ required: 'Full name is required', minLength: 2 }}
+        />
+        <button type="submit" disabled={isSubmitting} className="yellow-btn mt-5 w-full">
+          {isSubmitting ? 'Creating Account' : 'Start Your Investment Journey'}
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default SignUp;
